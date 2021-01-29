fa4b103fd082643560e251a381f5eb33130b493b
neto <garibaldiviolin@gmail.com>
2021-01-28 21:53:28 -0300

    Add BinaryTree traversals

--- src/tests/test_binary_trees.py
+++ src/tests/test_binary_trees.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from unittest.mock import call, patch
 
-from trees.binary_trees import BinaryTree
+from .fixtures import create_binary_tree
+from trees.binary_trees import (
+    BinaryTree,
+    post_order_traversal,
+    pre_order_traversal,
+    in_order_traversal,
+)
 
 
 class TestBinaryTreeClass(TestCase):
@@ -17,3 +24,34 @@ class TestBinaryTreeClass(TestCase):
 
     def test_str(self):
         self.assertEqual(repr(self.binary_tree), "BinaryTree(2)")
+
+
+class TestBinaryTreeTraversal(TestCase):
+    def setUp(self):
+        self.binary_tree = create_binary_tree()
+
+    @patch("builtins.print")
+    def test_post_order_traversal(self, print_mock):
+        post_order_traversal(self.binary_tree)
+        values_order = [1, 3, 2, 5, 7, 6, 4]
+        self.assertEqual(
+            print_mock.mock_calls,
+            [call(number, end="-") for number in values_order]
+        )
+
+    @patch("builtins.print")
+    def test_pre_order_traversal(self, print_mock):
+        pre_order_traversal(self.binary_tree)
+        values_order = [4, 2, 1, 3, 6, 5, 7]
+        self.assertEqual(
+            print_mock.mock_calls,
+            [call(number, end="-") for number in values_order]
+        )
+
+    @patch("builtins.print")
+    def test_in_order_traversal(self, print_mock):
+        in_order_traversal(self.binary_tree)
+        self.assertEqual(
+            print_mock.mock_calls,
+            [call(number, end="-") for number in range(1, 8)]
+        )
--- src/trees/binary_trees.py
+++ src/trees/binary_trees.py
@@ -10,3 +10,33 @@ class BinaryTree:
 
     def __str__(self):
         return str(self.value)
+
+
+def in_order_traversal(tree):
+    if tree.left is not None:
+        in_order_traversal(tree.left)
+
+    print(tree.value, end="-")
+
+    if tree.right is not None:
+        in_order_traversal(tree.right)
+
+
+def pre_order_traversal(tree):
+    print(tree.value, end="-")
+
+    if tree.left is not None:
+        pre_order_traversal(tree.left)
+
+    if tree.right is not None:
+        pre_order_traversal(tree.right)
+
+
+def post_order_traversal(tree):
+    if tree.left is not None:
+        post_order_traversal(tree.left)
+
+    if tree.right is not None:
+        post_order_traversal(tree.right)
+
+    print(tree.value, end="-")
